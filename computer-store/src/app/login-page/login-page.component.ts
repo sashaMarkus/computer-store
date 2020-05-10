beff89ad3b47ddf06cce7ebb78a3c6139c20fd1c
sashaMarkus <sashamarkus@hotmail.com>
2020-05-10 14:19:43 +0300

    login-page-fixed

--- computer-store/src/app/login-page/login-page.component.ts
+++ computer-store/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,20 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor()  {
+  loginForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder)  {
       
  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
+  get f() { return this.loginForm.controls };
 
-  onClick() {
-    
+  onSubmit() {
+    this.submitted = true;
+
+    if(this.loginForm.invalid) {
+      return;
+    }
   }
-
-  goToSignUpPage() {
-
-  }
-
 }
